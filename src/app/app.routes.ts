e70494f28a3881d480dc5d89718c1968d74ce848
FranRusG <135019380+FranRusG@users.noreply.github.com>
2023-11-10 12:59:05 +0100

    estructura principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+
   },
   {
     path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { MyComponentModule } from "../navbar/components.module";
+import { footerComponent } from '../footer/components.module';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    standalone: true,
+    imports: [IonHeader, IonToolbar, IonTitle, IonContent, MyComponentModule,footerComponent]
 })
 export class HomePage {
   constructor() {}
